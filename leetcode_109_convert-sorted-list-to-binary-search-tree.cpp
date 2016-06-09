8bdd8230f08d9f6cfa9dd817c272f0e4d20b4d81
lwl0810 <liuwenli0810@gmail.com>
2016-06-09 11:59:24 -0700

    Create leetcode_109_convert-sorted-list-to-binary-search-tree.cpp

---
+++ leetcode_109_convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+class Solution {
+public:
+//time O(n), space(1)
+TreeNode* sortedListToBST(ListNode*& head, int start, int end) {
+    if(start > end) return NULL;
+    int mid = (start + end) / 2;
+    TreeNode *left = sortedListToBST(head, start, mid-1);
+    TreeNode *root = new TreeNode(head->val);
+    head = head->next;
+    TreeNode *right= sortedListToBST(head, mid+1, end);
+    root->left = left;
+    root->right= right;
+    return root;
+}
+
+TreeNode* sortedListToBST(ListNode* head) {
+    int len = 0;
+    ListNode *p = head;
+    while (p) {
+        p = p->next;
+        len++;
+    }
+    return sortedListToBST(head, 0, len-1);
+}
+};
