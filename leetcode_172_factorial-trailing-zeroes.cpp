86c7a59db35066f2a506ba9b48cee6ee73e0a2a4
lwl0810 <liuwenli0810@gmail.com>
2016-06-10 00:18:45 -0700

    Create leetcode_172_factorial-trailing-zeroes.cpp

---
+++ leetcode_172_factorial-trailing-zeroes.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/factorial-trailing-zeroes/
+class Solution {
+public:
+//f(n!) = g(n!) = g(5^K * K! * A) = K + g(K!) = K + f(K!), K=n / 5
+//f(1000!) = 200 + f(200!) = 200 + 40 + f(40!) = 240 + 8 + f(8!) = 248 + 1 + f(1) =249
+int trailingZeroes(int n) {
+    int zeros = 0;
+    while(n > 0) {
+        n = n / 5;
+        zeros += n;
+    }
+    return zeros;
+}
+};
