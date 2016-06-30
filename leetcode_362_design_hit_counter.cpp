39c82343ae50038fec607aecdbcc9494f8cd4e9a
lwl0810 <liuwenli0810@gmail.com>
2016-06-29 21:27:05 -0700

    Update leetcode_362_design_hit_counter.cpp

--- leetcode_362_design_hit_counter.cpp
+++ leetcode_362_design_hit_counter.cpp
@@ -63,3 +63,39 @@ int HitCounter::getHits(int timestamp){
 	}
 	return q.size();
 }
+
+
+/*
+What if the number of hits per second could be very large? Does your design scale?
+
+class HitCounter {
+public:
+    HitCounter() {
+        times.resize(300);
+        hits.resize(300);
+    }
+    
+    void hit(int timestamp) {
+        int idx = timestamp % 300;
+        if (times[idx] != timestamp) {
+            times[idx] = timestamp;
+            hits[idx] = 1;
+        } else {
+            ++hits[idx];
+        }
+    }
+    
+    int getHits(int timestamp) {
+        int res = 0;
+        for (int i = 0; i < 300; ++i) {
+            if (timestamp - times[i] < 300) {
+                res += hits[i];
+            }
+        }
+        return res;
+    }
+
+private:
+    vector<int> times, hits;
+};
+*/
