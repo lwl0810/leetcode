e0c5afcbda45228922af9193aa48bf8a29f05379
lwl0810 <liuwenli0810@gmail.com>
2016-06-30 15:00:43 -0700

    Update leetcode_357_count-numbers-with-unique-digits.cpp

--- leetcode_357_count-numbers-with-unique-digits.cpp
+++ leetcode_357_count-numbers-with-unique-digits.cpp
@@ -4,6 +4,7 @@ class Solution {
 public:
     int countNumbersWithUniqueDigits(int k) {
         int res = 0;
+        //A(k,k)*C(n,k)
         //Let f(k) = count of numbers with unique digits with length equals k.
         //f(1) = 10, ..., f(k) = 9 * 9 * 8 * ... (9 - k + 2) [The first factor is 9 because a number cannot start with 0].
         if(k == 0) return 1;
