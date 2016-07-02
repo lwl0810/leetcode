ac7fa26d63277d60d0ae2738f551de17c035b4d0
lwl0810 <liuwenli0810@gmail.com>
2016-07-02 03:29:42 -0700

    Update leetcode_232_implement-queue-using-stacks.cpp

--- leetcode_232_implement-queue-using-stacks.cpp
+++ leetcode_232_implement-queue-using-stacks.cpp
@@ -1,5 +1,42 @@
 //https://leetcode.com/problems/implement-queue-using-stacks/
+class Queue {
+public:
+    // Push element x to the back of queue.
+    void push(int x) {
+        if(empty()) front = x;
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+        s1.push(x);
+    }
 
+    // Removes the element from in front of queue.
+    void pop(void) {
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s2.pop();
+        if(!empty()) front = s2.top();
+    }
+
+    // Get the front element.
+    int peek(void) {
+        return front;
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+        return s1.empty() && s2.empty();
+    }
+    
+private:
+    stack<int> s1, s2;
+    int front;
+};
+
+/*
 class Queue {
 public:
     // Push element x to the back of queue.
@@ -33,3 +70,4 @@ public:
 private:
     stack<int> s1, s2;
 };
+*/
