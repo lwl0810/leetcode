a4ff5fd57fd7bfef82c67b0edfd21c3d4f9e26aa
lwl0810 <liuwenli0810@gmail.com>
2016-07-04 02:18:09 -0700

    Create leetcode_069_sqrtx.cpp

---
+++ leetcode_069_sqrtx.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/sqrtx/
+
+class Solution {
+public:
+//binary search
+int mySqrt(int x) {
+    if(x < 0) return INT_MIN;
+    if(x == 0 || x == 1) return x;
+    int left = 1, right = x, mid = 0, factor = 0;
+    while (left <= right){
+        mid = left + (right - left) / 2;
+        factor = x / mid;
+        if (mid == factor) return mid;
+        else if(mid < factor) left = mid + 1;
+        else right = mid - 1;
+    }
+	return right;
+}
+
+//Newton: n2=(n1+x/n1)/2
+// int mySqrt(int x) {
+//     double n1=1, n2 = (n1 + x / n1) / 2;
+// 	while(floor(n1) != floor(n2)){
+// 	    n1 = n2;
+// 	    n2 = (n1 + x  /n1) / 2;
+// 	}
+// 	return floor(n1);
+// }
+};
