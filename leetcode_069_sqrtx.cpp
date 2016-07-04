0b2dbd838e5ba5e93b46fd9cd1f06a5c1c708197
lwl0810 <liuwenli0810@gmail.com>
2016-07-04 02:19:54 -0700

    Update leetcode_069_sqrtx.cpp

--- leetcode_069_sqrtx.cpp
+++ leetcode_069_sqrtx.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
 //binary search
 int mySqrt(int x) {
-    if(x < 0) return INT_MIN;
+	if(x < 0) return INT_MIN;
     if(x == 0 || x == 1) return x;
     int left = 1, right = x, mid = 0, factor = 0;
     while (left <= right){
@@ -12,17 +12,17 @@ int mySqrt(int x) {
         factor = x / mid;
         if (mid == factor) return mid;
         else if(mid < factor) left = mid + 1;
-        else right = mid - 1;
-    }
+		else right = mid - 1;
+	}
 	return right;
 }
 
 //Newton: n2=(n1+x/n1)/2
 // int mySqrt(int x) {
-//     double n1=1, n2 = (n1 + x / n1) / 2;
+// 	double n1=1, n2 = (n1 + x / n1) / 2;
 // 	while(floor(n1) != floor(n2)){
-// 	    n1 = n2;
-// 	    n2 = (n1 + x  /n1) / 2;
+// 		n1 = n2;
+// 		n2 = (n1 + x  /n1) / 2;
 // 	}
 // 	return floor(n1);
 // }
