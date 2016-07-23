1bab1daba56077995f06087601d6685cce4005f1
lwl0810 <liuwenli0810@gmail.com>
2016-07-22 17:33:47 -0700

    Create leetcode_159_longest_substring_with_at_most_two_distinct_characters.cpp

---
+++ leetcode_159_longest_substring_with_at_most_two_distinct_characters.cpp
@@ -0,0 +1,14 @@
+//http://www.danielbit.com/blog/puzzle/leetcode/leetcode-longest-substring-with-at-most-two-distinct-characters
+/*
+Given a string S, find the length of the longest substring T that contains at most two distinct characters.
+For example,
+Given S = “eceba”,
+T is “ece” which its length is 3.
+*/
+
+class Solution {
+public:
+int lengthOfLongestSubstringTwoDistinct(string s){
+  
+}
+};
