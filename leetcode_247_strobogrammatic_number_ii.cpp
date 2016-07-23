dcd7191181782057944ea9c39e7bec40936cdfb8
lwl0810 <liuwenli0810@gmail.com>
2016-07-23 15:38:17 -0700

    Update leetcode_247_strobogrammatic_number_ii.cpp

--- leetcode_247_strobogrammatic_number_ii.cpp
+++ leetcode_247_strobogrammatic_number_ii.cpp
@@ -10,6 +10,37 @@ Try to use recursion and notice that it should recurse with n - 2 instead of n -
 */
 
 class Solution{
+public:
+void strobogrammaticHelper(vector<string>& res, string strobo, string cur, int pos, int len, int isOdd){
+	if(cur.length() == len){
+		string secHalf = cur;
+		reverse(secHalf.begin(), secHalf.end());
+		for(int i = 0; i < len; ++i){
+			if(secHalf[i] == '6') secHalf[i] = '9';
+			else if(secHalf[i] == '9') secHalf[i] = '6';
+		}
+		if(isOdd){
+			for(int i = 0; i < 3; ++i){
+				if(i == 0 && len == 0) continue;
+				res.push_back(cur + strobo[i] + secHalf);
+			}
+		}else res.push_back(cur + secHalf);
+		return;
+	}
+	
+	for(int i = pos; i < strobo.length(); ++i){
+		if(cur.length() == 0 && i == 0) continue;
+		cur.push_back(strobo[i]);
+		strobogrammaticHelper(res, strobo, cur, i, len, isOdd);
+		cur.pop_back();
+	}
+}
+	
 vector<string> findStrobogrammatic(int n){
+	vector<string> res;
+	int len = n/2, isOdd = n%2;
+	string strobo = "01869";
+	strobogrammaticHelper(res, strobo, "", 0, len, isOdd);
+	return res;
 }
 };
