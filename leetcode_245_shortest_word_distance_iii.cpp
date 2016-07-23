45a37187d5eb2165fdd884205bb884e5a0503f9f
lwl0810 <liuwenli0810@gmail.com>
2016-07-23 00:02:58 -0700

    Create leetcode_245_shortest_word_distance_iii.cpp

---
+++ leetcode_245_shortest_word_distance_iii.cpp
@@ -0,0 +1,20 @@
+//http://buttercola.blogspot.com/2015/08/leetcode-shortest-word-distance-iii.html
+
+//This is a follow up of Shortest Word Distance. 
+//The only difference is now word1 could be the same as word2.
+//Given a list of words and two words word1 and word2, 
+//return the shortest distance between these two words in the list.
+//word1 and word2 may be the same and they represent two individual words in the list.
+//For example,
+//Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+//Given word1 = “makes”, word2 = “coding”, return 1.
+//Given word1 = "makes", word2 = "makes", return 3.
+//Note:
+//You may assume word1 and word2 are both in the list.
+
+class Solution {
+public:
+int shortestWordDistance(vector<string> words, string word1, string word2){
+
+}
+};
