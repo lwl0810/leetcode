d2495d9d09b11cc53d5237ad98d7f68495956fa3
lwl0810 <liuwenli0810@gmail.com>
2016-07-22 23:17:28 -0700

    Update leetcode_243_shortest_word_distance.cpp

--- leetcode_243_shortest_word_distance.cpp
+++ leetcode_243_shortest_word_distance.cpp
@@ -15,6 +15,16 @@ You may assume that word1 does not equal to word2, and word1 and word2 are both 
 class Solution {
 public:
 int shortestDistance(vector<string>& words, string word1, string word2) {
-
+	int sz = words.size(), idx1 = -1, idx2 = -1, dist = INT_MAX;
+	for(int i = 0; i < sz; ++i){
+		if(words[i] == word1){
+			idx1 = i;
+			if(idx2 != -1) dist = min(dist, abs(idx1 - idx2));
+		}else if(words[i] == word2){
+			idx2 = i;
+			if(idx1 != -1) dist = min(dist, abs(idx1 - idx2));
+		}	
+	}
+	return dist;
 }
 };
