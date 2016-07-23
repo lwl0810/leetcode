bf7b6c7d22e9486774eaf91acdba46fa9ce9d522
lwl0810 <liuwenli0810@gmail.com>
2016-07-22 21:34:58 -0700

    Update leetcode_163_missing_ranges.cpp

--- leetcode_163_missing_ranges.cpp
+++ leetcode_163_missing_ranges.cpp
@@ -8,7 +8,17 @@ For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4-
 
 class Solution {
 public:
+//time O(n), space O(n)
+//add lower-1 and upper+1 at the beginning and ending of nums to simplify code
 vector<string> findMissingRanges(vector<int> nums, int lower, int upper) {
-	
+	vector<string> res;
+	nums.push_back(upper+1);
+	int pre = lower-1, sz = nums.size();
+	for(int i = 0; i < sz; ++i){
+		if(nums[i] - pre == 2)	res.push_back(to_string(pre+1));
+		if(nums[i] - pre > 2)	res.push_back(to_string(pre+1) + "->" + to_string(nums[i]-1));
+		pre = nums[i];
+	}
+	return res;
 }
 };
