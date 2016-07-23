d4bccb837607c5c35295a1f23e57c43f8dc7575a
lwl0810 <liuwenli0810@gmail.com>
2016-07-23 14:45:02 -0700

    Create leetcode_247_strobogrammatic_number_ii.cpp

---
+++ leetcode_247_strobogrammatic_number_ii.cpp
@@ -0,0 +1,15 @@
+//http://blog.csdn.net/pointbreak1/article/details/48763175
+/*
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+Find all strobogrammatic numbers that are of length = n.
+
+For example,
+Given n = 2, return ["11","69","88","96"].
+Hint:
+Try to use recursion and notice that it should recurse with n - 2 instead of n - 1.
+*/
+
+class Solution{
+vector<string> findStrobogrammatic(int n){
+}
+};
