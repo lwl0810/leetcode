77893a0501aea39109bf257e1d0904c0044a407f
lwl0810 <liuwenli0810@gmail.com>
2016-07-22 22:47:30 -0700

    Create leetcode_186_reverse_words_in_string_ii.cpp

---
+++ leetcode_186_reverse_words_in_string_ii.cpp
@@ -0,0 +1,17 @@
+//http://betterpoetrythancode.blogspot.com/2015/02/reverse-words-in-string-ii-leetcode.html
+/*
+Reverse Words in a String II
+Question: Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
+The input string does not contain leading or trailing spaces and the words are always separated by a single space.
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+Could you do it in-place without allocating extra space?
+*/
+
+class Solution{
+public:
+void reverseWords(string &s){
+	
+}
+};
