3dc21c3cbd81f68281dc46f4ff901d857bc5ef8e
lwl0810 <liuwenli0810@gmail.com>
2016-07-22 20:39:00 -0700

    Update leetcode_159_longest_substring_with_at_most_two_distinct_characters.cpp

--- leetcode_159_longest_substring_with_at_most_two_distinct_characters.cpp
+++ leetcode_159_longest_substring_with_at_most_two_distinct_characters.cpp
@@ -8,7 +8,19 @@ T is “ece” which its length is 3.
 
 class Solution {
 public:
+//time O(n), space O(1)
+//i maintain the start index of sliding window, end-1 is the end of the window. ch is index of another distinct character
 int lengthOfLongestSubstringTwoDistinct(string s){
-  
+	int len = s.length(), longest = 0;
+	int start = 0, ch = -1;
+	for(int end = 1; end < len; ++end){
+		if(s[end] == s[end-1]) continue;
+		if(ch >= 0 && s[ch] != s[end]){
+			longest = max(longest, end-1-start+1);
+			start = ch+1;
+		}
+		ch = end-1;
+	}
+	return max(longest, end - start);
 }
 };
