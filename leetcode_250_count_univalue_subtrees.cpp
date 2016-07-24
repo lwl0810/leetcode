4a45d92ddf7d5f8b08f7f38616faf53805d88cdd
lwl0810 <liuwenli0810@gmail.com>
2016-07-24 15:57:50 -0700

    Create leetcode_250_count_univalue_subtrees.cpp

---
+++ leetcode_250_count_univalue_subtrees.cpp
@@ -0,0 +1,18 @@
+//http://www.voidcn.com/blog/pointbreak1/article/p-4357160.html
+/*
+Count Univalue Subtrees
+Given a binary tree, count the number of uni-value subtrees.
+A Uni-value subtree means all nodes of the subtree have the same value.
+
+For example:
+Given binary tree,
+              5
+             / \
+            1   5
+           / \   \
+          5   5   5
+return 4.
+*/
+
+class Solution{
+};
