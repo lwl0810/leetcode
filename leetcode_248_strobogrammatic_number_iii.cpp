d1870937ec50cd4f6edee4d3ec622b5d1c27d5c6
lwl0810 <liuwenli0810@gmail.com>
2016-07-24 00:29:47 -0700

    Update leetcode_248_strobogrammatic_number_iii.cpp

--- leetcode_248_strobogrammatic_number_iii.cpp
+++ leetcode_248_strobogrammatic_number_iii.cpp
@@ -11,7 +11,25 @@ Because the range might be a large number, the low and high numbers are represen
 
 class Solution{
 public:
-int strobogrammaticInRange(string low, string high){
+void strobogrammaticHelper(string& low, string& high, string cur, int& cnt){
+	if(cur.length() >= low.length() && cur.length() <= high.length()){
+		if(cur.length() == low.length() && cur < low || cur.length() == high.length() && cur > high) return;
+		if(!(cur.length() > 1 && cur[0] == '0')) ++cnt;
+	}
+	if(cur.length() + 2 > high.length()) return;
+	strobogrammaticHelper(low, high, '0' + cur + '0', cnt);
+	strobogrammaticHelper(low, high, '1' + cur + '1', cnt);
+	strobogrammaticHelper(low, high, '8' + cur + '8', cnt);
+	strobogrammaticHelper(low, high, '6' + cur + '9', cnt);
+	strobogrammaticHelper(low, high, '9' + cur + '6', cnt);
+}
 
+int strobogrammaticInRange(string low, string high){
+	int cnt = 0;
+	strobogrammaticHelper(low, high, "", cnt);
+	strobogrammaticHelper(low, high, "0", cnt);
+	strobogrammaticHelper(low, high, "1", cnt);
+	strobogrammaticHelper(low, high, "8", cnt);
+	return cnt;
 }
 };
