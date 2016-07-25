a3f87c5c86bf7e71f8b4c6856e93f9ff615fc988
lwl0810 <liuwenli0810@gmail.com>
2016-07-24 17:55:17 -0700

    Create leetcode_251_flatten_2d_vector.cpp

---
+++ leetcode_251_flatten_2d_vector.cpp
@@ -0,0 +1,16 @@
+//http://www.cnblogs.com/jcliBlogger/p/4704781.html
+/*
+Flatten 2D Vector
+Implement an iterator to flatten a 2d vector.
+For example,
+Given 2d vector =
+[
+  [1,2],
+  [3],
+  [4,5,6]
+]
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1, 2, 3, 4, 5, 6].
+*/
+
+class Solution{
+};
