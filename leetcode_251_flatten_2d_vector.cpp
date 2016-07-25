f0a2be73487a2ff691804887e25ddc5d8f9328e8
lwl0810 <liuwenli0810@gmail.com>
2016-07-24 18:18:43 -0700

    Update leetcode_251_flatten_2d_vector.cpp

--- leetcode_251_flatten_2d_vector.cpp
+++ leetcode_251_flatten_2d_vector.cpp
@@ -12,5 +12,28 @@ Given 2d vector =
 By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1, 2, 3, 4, 5, 6].
 */
 
-class Solution{
+class Vector2D{
+public:
+Vector2D(vector<vector<int>>& vec2d){
+	vec = vec2d;
+	it = vec.begin();
+	pos = 0;
+}
+
+int next(){
+	return (*it)[pos++];
+}
+
+bool hasNext(){
+	while(it != vec.end() && pos == (*it).size()) {
+		++it;
+		pos = 0;
+	}
+	return it != vec.end();
+}
+
+private:
+	vector<vector<int>> vec;
+	vector<vector<int>>::iterator it;
+	int pos;
 };
