da5a48fa072fc75807d2e33d1505027008909386
lwl0810 <liuwenli0810@gmail.com>
2016-07-25 00:17:30 -0700

    Update leetcode_255_verify_preorder_sequence_in_binary_search_tree.cpp

--- leetcode_255_verify_preorder_sequence_in_binary_search_tree.cpp
+++ leetcode_255_verify_preorder_sequence_in_binary_search_tree.cpp
@@ -10,5 +10,31 @@ Could you do it using only constant space complexity?
 
 calss Solution{
 public:
+//time O(n), space O(n)
+bool verifyPreorder(vector<int>& preorder){
+	stack<int> s;
+	int lower = INT_MIN;
+	for(int i = 0; i <  preorder.size(); ++i){
+		if(preorder[i] < lower) return false;
+		while(!s.empty() && s.top() < preorder[i]){
+			lower = s.top();
+			s.pop();
+		}
+		s.push(preorder[i]);
+	}
+	return true;
+}
 
+//follow up, space O(1), reuse the input vector as stack, stacktop is the stack top indicator.
+bool verifyPreorder(vector<int>& preorder){
+	int stacktop = -1, lower = INT_MIN;
+	for(int i = 0; i < preorder.size(); ++i){
+		if(preorder[i] < lower) return false;
+		while(stacktop != -1 && preorder[stacktop] < preorder[i]){
+			lower = s[stacktop--];
+		}
+		s[++stacktop] = preorder[i];
+	}
+	return true;
+}
 };
