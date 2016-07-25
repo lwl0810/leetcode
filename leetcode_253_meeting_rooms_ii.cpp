96879ba9ccdc90a2486927cb4a97651eb565eba6
lwl0810 <liuwenli0810@gmail.com>
2016-07-24 20:02:15 -0700

    Update leetcode_253_meeting_rooms_ii.cpp

--- leetcode_253_meeting_rooms_ii.cpp
+++ leetcode_253_meeting_rooms_ii.cpp
@@ -8,6 +8,30 @@ Given [[0, 30],[5, 10],[15, 20]],
 return 2.
 */
 
-class Solution{
+class Interval {
+public:
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
+};
 
+class Solution{
+public:
+bool compare(Interval& a, Interval& b){
+	return a.start < b.start;
+}
+
+//time O(nlogn), space O(n)
+int minMeetingRooms(vector<Interval>& intervals){
+	int minRooms = 0;
+	priority_queue<int, vector<int>, greater<int>> inUse; //end time of inuse interval
+	sort(intervals.begin(), intervals.end(), compare);
+	for(int i = 0; i < intervals.size(); ++i){
+		while(!inUse.empty() && inUse.top() <= intervals[i].start) inUse.pop();
+		inUse.push(intervals[i].end);
+		minRooms = max(minRooms, inUse.size());
+	}
+	return minRooms;
+}
 };
