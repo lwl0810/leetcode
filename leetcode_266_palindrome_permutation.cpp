1758ef7ee987675e2f37d69d1ce88a269ee7bf2c
lwl0810 <liuwenli0810@gmail.com>
2016-07-25 13:29:25 -0700

    Create leetcode_266_palindrome_permutation.cpp

---
+++ leetcode_266_palindrome_permutation.cpp
@@ -0,0 +1,17 @@
+//http://www.cnblogs.com/jcliBlogger/p/4748554.html
+/*
+Palindrome Permutation
+Given a string, determine if a permutation of the string could form a palindrome.
+For example,
+"code" -> False, "aab" -> True, "carerac" -> True.
+Hint:
+Consider the palindromes of odd vs even length. What difference do you notice?
+Count the frequency of each character.
+If each character occurs even number of times, then it must be a palindrome. How about character which occurs odd number of times?
+*/
+
+class Solution{
+public:
+bool canPermutePalindrome(string s){
+}
+};
