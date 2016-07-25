625b5ea4b77d3f1f7e6176ed21e953860d9c5d3e
lwl0810 <liuwenli0810@gmail.com>
2016-07-24 17:52:12 -0700

    Update leetcode_250_count_univalue_subtrees.cpp

--- leetcode_250_count_univalue_subtrees.cpp
+++ leetcode_250_count_univalue_subtrees.cpp
@@ -15,4 +15,55 @@ return 4.
 */
 
 class Solution{
+public:
+//iteration solution
+int countUnivalSubtrees(TreeNode* root){
+	unordered_map<TreeNode*, int> isUnival;
+	stack<TreeNode*> s;
+	int cnt = 0;
+	while(root || !s.empty()){
+		while(root){
+			s.push(root);
+			isUnival[root] = 0;  //0:visited, -1 or 1:visited
+			root = root->left;
+		}
+		if(!s.empty()){
+			root = s.top();
+			if(isUnival[root] == 0){
+				isUnival[root] = -1;
+				root = root->right;
+			}else{
+				s.pop();
+				if(	(!root->left || isUnival[root->left] == 1 && root->left->val == root->val) && 
+					(!root->right || isUnival[root->right] == 1 && root->right->val == root->val)){
+					isUnival[root] = 1;
+					++cnt;
+				}else{
+					isUnival[root] = -1;
+				}
+				root = NULL;
+			}
+		}
+	}
+	return cnt;
+}
+
+//recursion solution
+bool countHelper(TreeNode* root, int &cnt){
+	if(!root) return true;
+	if(countHelper(root->left) && countHelper(root->right)){
+		if( (!root->left  || root->val == root->left->val) && 
+			(!root->right || root->val == root->right->val)){
+			++cnt;
+			return true;
+		}
+	}
+	return false;
+}
+
+int countUnivalSubtrees(TreeNode* root){
+	int cnt = 0;
+	countHelper(root, cnt);
+	return cnt;
+}
 };
