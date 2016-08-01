23e9acf4c787a1d7ee59eb03c0d3712aafafb41d
lwl0810 <liuwenli0810@gmail.com>
2016-08-01 16:36:44 -0700

    Update leetcode_277_find_the_celebrity.cpp

--- leetcode_277_find_the_celebrity.cpp
+++ leetcode_277_find_the_celebrity.cpp
@@ -3,7 +3,7 @@
 /*
 Find the Celebrity 
 Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. 
-The definition of a celebrity is that all the other n - 1people know him/her but he/she does not know any of them.
+The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
 
 Now you want to find out who the celebrity is or verify that there is not one. 
 The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. 
@@ -15,10 +15,20 @@ Implement a function int findCelebrity(n), your function should minimize the num
 Note: There will be exactly one celebrity if he/she is in the party. 
 Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
 */
+bool knows(int a, int b);
 
 class Solution{
 public:
+//time O(n) space O(1)
 int findCelebrity(int n) {
-
+	int l = 0, r = n - 1;
+	while (l < r) {
+		if (knows(l, r)) ++l;
+		else --r;
+	}
+	for (int i = 0; i < n; ++i) if (i != l) {
+		if (knows(l, i) || !knows(i, l)) return -1;
+	}
+	return l;
 }
 };
