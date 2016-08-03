347dcee1ca7306731d36dfbafe8e266e1586d9c5
lwl0810 <liuwenli0810@gmail.com>
2016-08-02 23:47:46 -0700

    Create leetcode_288_unique_word_abbreviation.cpp

---
+++ leetcode_288_unique_word_abbreviation.cpp
@@ -0,0 +1,46 @@
+//http://www.cnblogs.com/jcliBlogger/p/4851799.html
+/*
+An abbreviation of a word follows the form <first letter><number><last letter>. 
+Below are some examples of word abbreviations:
+a) it                      --> it    (no abbreviation)
+
+     1
+b) d|o|g                   --> d1g
+
+              1    1  1
+     1---5----0----5--8
+c) i|nternationalizatio|n  --> i18n
+
+              1
+     1---5----0
+d) l|ocalizatio|n          --> l10n
+
+Assume you have a dictionary and given a word, find whether its abbreviation is unique in the dictionary. 
+A word's abbreviation is unique if no other word from the dictionary has the same abbreviation.
+
+Example: 
+Given dictionary = [ "deer", "door", "cake", "card" ]
+isUnique("dear") -> false
+isUnique("cart") -> true
+isUnique("cane") -> false
+isUnique("make") -> true 
+*/
+
+class ValidWordAbbr {
+public:
+ValidWordAbbr(vector<string> &dictionary) {
+
+}
+
+bool isUnique(string word) {
+
+}
+private:
+unordered_map<string, unordered_set<string>> mp;
+};
+
+
+// Your ValidWordAbbr object will be instantiated and called as such:
+// ValidWordAbbr vwa(dictionary);
+// vwa.isUnique("hello");
+// vwa.isUnique("anotherWord"); 
