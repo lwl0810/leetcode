ff63dfc642d6b854d27fa6d9f6adf3678e83c1b2
lwl0810 <liuwenli0810@gmail.com>
2016-08-03 14:32:06 -0700

    Update leetcode_314_binary_tree_vertical_order_traversal.cpp

--- leetcode_314_binary_tree_vertical_order_traversal.cpp
+++ leetcode_314_binary_tree_vertical_order_traversal.cpp
@@ -1,6 +1,6 @@
-//
+//http://blog.csdn.net/u012175043/article/details/50201133
 /*
-
+Binary Tree Vertical Order Traversal 
 Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
 If two nodes are in the same row and column, the order should be from left to right.
 
@@ -9,8 +9,8 @@ Given binary tree [3,9,20,null,null,15,7],
         3
        / \
       9  20
-      /   \
-     15   7
+        /   \
+       15   7
 return its vertical order traversal as:
     [
       [9],
@@ -36,3 +36,26 @@ Hide Company Tags Facebook
 Hide Tags Hash Table
 Hide Similar Problems (E) Binary Tree Level Order Traversal
 */
+
+class Solution{
+public:
+//time O(nlogn) space O(n)
+vector<vector<int>> verticalOrder(TreeNode* root) {
+	map<int, vector<int>> m;
+	vector<vector<int>> res;
+	queue<pair<int, TreeNode*>> q;
+	q.push(make_pair(0, root));
+	while(!q.empty()){
+		int idx = q.front().first;
+		TreeNode *cur = q.front().second;
+		m[idx].push_back(cur->val);
+		q.pop();
+		if(cur->left) q.push(make_pair(idx-1, cur->left));
+		if(cur->right)q.push(make_pair(idx+1, cur->right));
+	}
+	for(map<int,vector<int>>::iterator it = m.begin(); it != m.end(); ++it){
+		res.push_back(it->second);
+	}
+	return res;
+}
+};
