1a472a16014747b142b9e3f12df5332a0c215817
lwl0810 <liuwenli0810@gmail.com>
2016-08-03 16:05:16 -0700

    Create leetcode_325_maximum_size_subarray_sum_equals_k.cpp

---
+++ leetcode_325_maximum_size_subarray_sum_equals_k.cpp
@@ -0,0 +1,19 @@
+//http://codingmelon.com/2016/01/04/maximum-size-subarray-sum-equals-k-leetcode-325/
+/*
+Maximum Size Subarray Sum Equals k
+Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn’t one, return 0 instead.
+Example 1:
+Given nums = [1, -1, 5, -2, 3], k = 3,
+return 4. (because the subarray [1, -1, 5, -2] sums to 3 and is the longest)
+
+Example 2:
+Given nums = [-2, -1, 2, 1], k = 1,
+return 2. (because the subarray [-1, 2] sums to 1 and is the longest)
+
+Follow Up:
+Can you do it in O(n) time?
+*/
+
+class Solution{
+public:
+};
