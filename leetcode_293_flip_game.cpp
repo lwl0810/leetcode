1c57975582921b33d9d3922213e0d21621d9f575
lwl0810 <liuwenli0810@gmail.com>
2016-08-03 22:40:56 -0700

    Update leetcode_293_flip_game.cpp

--- leetcode_293_flip_game.cpp
+++ leetcode_293_flip_game.cpp
@@ -19,5 +19,15 @@ If there is no valid move, return an empty list [].
 class Solution {
 public:
 vector<string> generatePossibleNextMoves(string s) {
+	vector<string> res;
+	int len = s.length();
+	for(int i = 1; i < len; ++i){
+		if(s[i] == '+' && s[i-1] == '+'){
+			s[i] = '-'; s[i-1] = '-';
+			res.push_back(s);
+			s[i] = '+'; s[i-1] = '+';
+		}
+	}
+	return res;
 }
 };
