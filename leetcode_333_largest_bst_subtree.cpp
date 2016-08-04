bc3f375817ee992f640541a92353468c778ea156
lwl0810 <liuwenli0810@gmail.com>
2016-08-03 17:09:17 -0700

    Create leetcode_333_largest_bst_subtree.cpp

---
+++ leetcode_333_largest_bst_subtree.cpp
@@ -0,0 +1,21 @@
+//http://codingmelon.com/2016/02/13/largest-bst-subtree-leetcode-333/
+/*
+Largest BST Subtree
+Given a binary tree, find the largest subtree which is a Binary Search Tree (BST), 
+where largest means subtree with largest number of nodes in it.
+Note:
+A subtree must include all of its descendants.
+Here’s an example:
+    10
+    / \
+   5  15
+  / \   \ 
+ 1   8   7
+
+The Largest BST Subtree in this case is the highlighted one.
+The return value is the subtree’s size, which is 3.
+*/
+
+class Solution{
+public:
+};
