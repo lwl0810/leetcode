080d256b92149601cab7ff89fa5f6dceaea111af
lwl0810 <liuwenli0810@gmail.com>
2016-08-04 12:48:55 -0700

    Update leetcode_340_longest_substring_with_at_most_k_distinct_characters.cpp

--- leetcode_340_longest_substring_with_at_most_k_distinct_characters.cpp
+++ leetcode_340_longest_substring_with_at_most_k_distinct_characters.cpp
@@ -6,3 +6,22 @@ Given a string, find the length of the longest substring T that contains at most
 For example, Given s = “eceba” and k = 2,
 T is "ece" which its length is 3.
 */
+
+class Solution {
+public:
+//time O(n) space O(k)
+int lengthOfLongestSubstringKDistinct(string s, int k) {
+	unordered_map<char, int> um;
+	int maxLen = 0;
+	for(int l = 0, r = 0; r < s.length(); ++r) {
+		um[s[r]]++;
+		while(um.size() > k){
+			if(um[s[l]] == 1) um.erase[s[l]];
+			else um[s[l]]--;
+			++l;
+		}
+		maxLen = max(maxLen, r-l+1);
+	}
+	return maxLen;
+}
+};
