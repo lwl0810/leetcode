2e4752e5fd8dee3dc9b09f048138e8b4710c0d85
lwl0810 <liuwenli0810@gmail.com>
2016-08-04 12:38:10 -0700

    Create leetcode_340_longest_substring_with_at_most_k_distinct_characters.cpp

---
+++ leetcode_340_longest_substring_with_at_most_k_distinct_characters.cpp
@@ -0,0 +1,8 @@
+//http://www.cnblogs.com/grandyang/p/5351347.html
+/*
+Longest Substring with At Most K Distinct Characters
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+For example, Given s = “eceba” and k = 2,
+T is "ece" which its length is 3.
+*/
