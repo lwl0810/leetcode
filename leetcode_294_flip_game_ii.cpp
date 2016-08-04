c163f1bb2e554106c28a77ea181ce1c591298797
lwl0810 <liuwenli0810@gmail.com>
2016-08-03 23:01:41 -0700

    Update leetcode_294_flip_game_ii.cpp

--- leetcode_294_flip_game_ii.cpp
+++ leetcode_294_flip_game_ii.cpp
@@ -11,3 +11,15 @@ For example, given s = "++++", return true. The starting player can guarantee a 
 Follow up:
 Derive your algorithm's runtime complexity.
 */
+
+class Solution {
+public:
+//time O(2^n)
+//DP solution: https://discuss.leetcode.com/topic/27282/theory-matters-from-backtracking-128ms-to-dp-0ms
+bool canWin(string s) {
+	for(int i = -1; (i = s.find("++", i+1)) >= 0; )
+		if(!canWin(s.substr(0, i) + "--" + s.substr(i+2)))
+			return true;
+	return false;
+}
+};
